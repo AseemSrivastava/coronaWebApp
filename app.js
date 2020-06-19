40c6ca129012f4bc8f0aea53cadbee8e1ebad402
bhusalashish <ashish.bhusal.16999@gmail.com>
2020-06-19 12:23:23 +0530

    add all countries

--- app.js
+++ app.js
@@ -12,16 +12,18 @@ app.use(express.static("public"));
 
 
 app.get("/", function(req, res){
-    api.countries({country: 'india' }).then(function(result){
-        res.render("index", {data : result});
+    api.countries().then(function(result){
+        res.render("index", {datas : result});
         console.log(result);
+        
+    }).catch(function(err){
+        console.log(err);
         
     });
 });
 
 
 app.listen(3000, function(){
-    api.all().then(console.log);
     console.log("Server started on port 3000");
     
 });
