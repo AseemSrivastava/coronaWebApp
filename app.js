5681c2ee56238ae30b964bb82d8b91b6c101e7e1
AseemSrivastava <aseemsrivastava9@gmail.com>
2020-06-20 16:35:41 +0530

    chnaged somethings in app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
-const api = require('novelcovid')
+const api = require('novelcovid');
 const request = require('request');
-const app = express();
+const app = express();  
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended : true}));
@@ -13,18 +13,41 @@ app.use(express.static("public"));
 app.get("/", function(req, res){
     api.countries().then(function(result){
         res.render("index", {datas : result});
-        console.log(result);
-        
     }).catch(function(err){
         console.log(err);
-        
     });
 });
 
 app.get("/news", function(req, res){
         request('https://covid-19india-api.herokuapp.com/headlines', function (error, response, body) {
-          res.render("news",{datas : body})
-          console.log('body:', body);
+          var content = JSON.parse(body);
+          res.render("news",{datas : content});
+          console.log('body:', content);
+    });
+});
+var fs = require('fs');
+app.get("/map", function(req, res){
+  api.countries().then(function(result){
+        var arr = [];
+        result.forEach(function(data){
+          var obj = {
+          lat: data.countryInfo.lat,
+          long: data.countryInfo.long,
+          cases: data.cases
+        }
+          arr.push(obj);
+        });
+        res.render("worldmap", {datas : arr});
+        console.log(arr);
+        fs.writeFile("/temp", arr, function(err){
+          if(err){
+              console.log(err);
+          } else{
+              console.log("success");
+          }
+        });
+    }).catch(function(err){
+        console.log(err);
     });
 });
 
